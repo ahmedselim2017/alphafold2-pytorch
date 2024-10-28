8f82a3936b329074f1d9147f64fe7e01a796c8cc
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-10-28 04:00:31 +0300

    added EvoformerStack and performed some small fixes

--- alphafold/model/evoformer.py
+++ alphafold/model/evoformer.py
@@ -45,12 +45,64 @@ class EvoformerBlock(torch.nn.Module):
             PyTorch tensors with the shapes of m and z.
         """
 
-        m += self.dropout_rowwise_m(self.msa_att_row(m, z))
-        m += self.msa_att_col(m)
-        m += self.msa_transition(m)
+        m = m + self.dropout_rowwise_m(self.msa_att_row(m, z))
+        m = m + self.msa_att_col(m)
+        m = m + self.msa_transition(m)
 
-        z += self.outer_product_mean(m)
+        z = z + self.outer_product_mean(m)
 
         z = self.core(z)
 
         return m, z
+
+
+class EvoformerStack(torch.nn.Module):
+    """
+    Implementation of the Evoformer stack from the Algorithm 6.
+    """
+
+    def __init__(self,
+                 c_m: int,
+                 c_z: int,
+                 num_blocks: int = 48,
+                 c_s: int = 384):
+        """
+        Initializes the Evoformer stack.
+
+        Args:
+            c_m:    Embedding dimension for the MSA representation.
+            c_z:    Embedding dimension for the pairwise representation.
+                    num_blocks: Number of Evoformer blocks that should be used.
+            c_s:    Number of channels that should be used for the single
+                    representation.
+        """
+
+        super().__init__()
+
+        self.blocks = torch.nn.ModuleList(
+            [EvoformerBlock(c_m, c_z) for _ in range(num_blocks)])
+        self.linear = torch.nn.Linear(c_m, c_s)
+
+    def forward(
+            self, m: torch.Tensor, z: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Forward pass for the Evoformer stack.
+
+        Args:
+            m:  A PyTorch tensor of shape (*, N_seq, N_res, c_m) that contains
+                the MSA representation.
+            z:  A PyTorch tensor of shape (*, N_res, N_res, c_m) that contains
+                the pairwise representation.
+
+        Returns:
+            Output tensors with the same shape as m, z and the single
+            representation tensor with the shape of (*, N_res, c_s).
+
+        """
+
+        for evoformer_block in self.blocks:
+            m, z = evoformer_block(m, z)
+        s = self.linear(m[..., 0, :, :])
+
+        return m, z, s
--- tests/test_evoformer.py
+++ tests/test_evoformer.py
@@ -12,3 +12,19 @@ def test_EvoformerBlock():
 
     test_module(evo_block, 'evo_block', ('m', 'z'), ('m_out', 'z_out'),
                 control_folder)
+
+
+def test_EvoformerStack():
+    from alphafold.model.evoformer import EvoformerStack
+    from tests.control_values.evoformer.evoformer_checks import c_m, c_z
+    from tests.control_values.evoformer.evoformer_checks import test_module_shape, test_module
+
+    num_blocks = 3
+    c_s = 5
+
+    evoformer = EvoformerStack(c_m, c_z, num_blocks, c_s)
+
+    test_module_shape(evoformer, 'evoformer', control_folder)
+
+    test_module(evoformer, 'evoformer', ('m', 'z'),
+                ('m_out', 'z_out', 's_out'), control_folder)
