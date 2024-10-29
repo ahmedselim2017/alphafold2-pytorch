a11f7e18ad4ae4219b45d4504228f7f365705acc
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-10-29 06:00:44 +0300

    small fixes and added quat_from_axis and quat_mul

--- alphafold/utils/geometry.py
+++ alphafold/utils/geometry.py
@@ -1,15 +1,15 @@
 import torch
 
 
-def create_3x3_rotation(ex: torch.Tensor, ey: torch.Tensor):
+def create_3x3_rotation(ex: torch.Tensor, ey: torch.Tensor) -> torch.Tensor:
     """
     Creates a rotation matrix by orthonormalizing the ex and ey vectors
     using Gram-Schmidt orthonormalization.
 
     Args:
-        ex: A PyTorch tensor of shape (*, 3).
-        ey: A PyTorch tensor of shape (*, 3).
-    
+        ex: A PyTorch tensor with a shape of (*, 3).
+        ey: A PyTorch tensor with a shape of (*, 3).
+
     Returns:
         A PyTorch tensor of shape (*,3,3).
     """
@@ -20,3 +20,49 @@ def create_3x3_rotation(ex: torch.Tensor, ey: torch.Tensor):
     z = torch.linalg.cross(x, y, dim=-1)
 
     return torch.stack((x, y, z), dim=-1)
+
+
+def quat_from_axis(phi: torch.Tensor, n: torch.Tensor) -> torch.Tensor:
+    """
+    Creates a quaternion with a scalar cos(phi/2) and a vector sin(phi/2)*n
+
+    Args:
+        phi:    A PyTorch tensor with a shape of (*,).
+        n:  A PyTorch tensor with a shape of (*, 3).
+
+    Returns:
+        A PyTorch tensor with a shape of (*, 4)
+    """
+
+    a = torch.cos(phi / 2).unsqueeze(-1)
+    v = torch.sin(phi / 2).unsqueeze(-1) * n
+
+    return torch.cat((a, v), dim=-1)
+
+
+def quat_mul(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
+    """
+    Multiplies the given quaternions.
+
+    Args:
+        q1: A PyTorch tensor with a shape of (*,4) that includes the first
+            quaternion
+        q2: A PyTorch tensor with a shape of (*,4) that includes the second
+            quaternion
+    Returns:
+        A PyTorch tensor of shape (*,4) that is the multiplication of q1 and q2
+    """
+
+    # q3 = (c,u)
+    # q3 = q1*q2 = (a,v) * (b,w) = (ab - v dot w, aw + bv + v cross w)
+
+    a = q1[..., 0:1]  # (*, 1)
+    v = q1[..., 1:]  # (*, 3)
+
+    b = q2[..., 0:1]  # (*, 1)
+    w = q2[..., 1:]  # (*, 3)
+
+    c = a * b - torch.sum(v * w, dim=-1, keepdim=True)
+    u = a * w + b * v + torch.linalg.cross(v, w, dim=-1)
+
+    return torch.cat((c, u), dim=-1)
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,4 +1,7 @@
 import torch
+import math
+
+control_folder = "./control_values/geometry"
 
 
 def test_create_3x3_rotatiton():
@@ -20,3 +23,37 @@ def test_create_3x3_rotatiton():
     assert torch.allclose(R, R_exp, atol=1e-3), 'Error in single computation.'
     assert torch.allclose(R_batch, R_exp_batch,
                           atol=1e-3), 'Error in batched computation.'
+
+
+def test_quat_mul():
+    from alphafold.utils.geometry import quat_from_axis, quat_mul
+
+    phi = torch.tensor(math.pi / 4)
+    n = torch.tensor([0.2, 0.5, -0.3])
+    n = n / torch.linalg.vector_norm(n)
+
+    phi_batch = phi.broadcast_to(2, 5)
+    n_batch = n.broadcast_to(2, 5, 3)
+
+    q = quat_from_axis(phi, n)
+    q_batch = quat_from_axis(phi_batch, n_batch)
+
+    q_exp = torch.load(f'{control_folder}/quat_from_axis_check.pt')
+
+    assert torch.allclose(q, q_exp,
+                          atol=1e-5), 'Error in quat_from_axis, single use.'
+    assert torch.allclose(q_batch, q_exp.broadcast_to((2, 5, 4)),
+                          atol=1e-5), 'Error in quat_from_axis, batched use.'
+
+    p = torch.tensor([0.3, -0.4, 0.1, 0.8])
+    p_batch = p.broadcast_to(2, 5, 4)
+
+    pq = quat_mul(p, q)
+    pq_batch = quat_mul(p_batch, q_batch)
+
+    pq_exp = torch.load(f'{control_folder}/quat_mul_check.pt')
+
+    assert torch.allclose(pq, pq_exp,
+                          atol=1e-5), 'Error in quat_mul, single use.'
+    assert torch.allclose(pq_batch, pq_exp.broadcast_to((2, 5, 4)),
+                          atol=1e-5), 'Error in quat_mul, batched use.'
