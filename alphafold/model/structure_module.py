7e23635919cc6193093f7ae3d80299860cbb7807
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-10-30 22:49:21 +0300

    added BackboneUpdate

--- alphafold/model/structure_module.py
+++ alphafold/model/structure_module.py
@@ -1,5 +1,7 @@
 import torch
 
+from alphafold.utils.geometry import quat_to_3x3_rotation, assemble_4x4_transform
+
 
 class StructureModuleTransition(torch.nn.Module):
     """
@@ -15,8 +17,6 @@ class StructureModuleTransition(torch.nn.Module):
         """
 
         super().__init__()
-
-        self.c_s = c_s
 
         self.linear_1 = torch.nn.Linear(c_s, c_s)
         self.linear_2 = torch.nn.Linear(c_s, c_s)
@@ -41,3 +41,44 @@ class StructureModuleTransition(torch.nn.Module):
             self.relu(self.linear_2(self.relu(self.linear_1(s)))))
         s = self.layer_norm(self.dropout(s))
         return s
+
+
+class BackboneUpdate(torch.nn.Module):
+    """
+    Implementation of the Algorithm 23.
+    """
+
+    def __init__(self, c_s: int):
+        """
+        Initializes the BackboneUpdate.
+
+        Args:
+            c_s:    Dimension of the single representation.
+        """
+
+        super().__init__()
+
+        self.linear = torch.nn.Linear(c_s, 6)
+
+    def forward(self, s: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the BackboneUpdate.
+
+        Args:
+            s:  A PyTorch tensor with a shape of (*, N_res, c_s) that includes
+                the single representation.
+        """
+
+        outs = self.linear(s)
+
+        quat_ones = torch.ones(s.shape[:-1] + (1, ),
+                               device=s.device,
+                               dtype=s.dtype)
+
+        quat = torch.cat((quat_ones, outs[..., :3]), dim=-1)
+        quat = quat / torch.linalg.vector_norm(quat, dim=-1, keepdim=True)
+
+        R = quat_to_3x3_rotation(quat)
+        t = outs[..., 3:]
+
+        return assemble_4x4_transform(R, t)
--- tests/test_structure_module.py
+++ tests/test_structure_module.py
@@ -11,3 +11,14 @@ def test_StructureModuleTransition():
 
     test_module_forward(transition, 'sm_transition', 's', 's_out',
                         control_folder)
+
+
+def test_BackboneUpdate():
+    from alphafold.model.structure_module import BackboneUpdate
+    from tests.control_values.structure_module.structure_module_checks import test_module_forward, test_module_shape, c_s
+
+    bb_update = BackboneUpdate(c_s)
+
+    test_module_shape(bb_update, 'bb_update', control_folder)
+
+    test_module_forward(bb_update, 'bb_update', 's', 'T_out', control_folder)
